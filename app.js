1e68cb6ad92b6dace15d1fed55eb7975e3a9f992
Kaushik Kumar Sahoo <sahookaushik1797@gmail.com>
2025-07-29 03:42:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,4 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
