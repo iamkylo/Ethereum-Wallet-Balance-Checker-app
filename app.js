6bf366748780934427f2873a8b4aec805eb0090b
iamkylo <sahookaushik1797@gmail.com>
2025-07-29 03:39:27 +0530

    fix: export app for Vercel

--- app.js
+++ app.js
@@ -17,8 +17,5 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// ADD THIS LINE AT THE END
+module.exports = app;
